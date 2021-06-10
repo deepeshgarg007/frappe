3550ac4ab54388d005ee6cdf9cdffa3184377623
Deepesh Garg <deepeshgarg6@gmail.com>
2021-06-10 17:57:25 +0530

    fix: Patch due to language doctype not being found

--- frappe/patches/v10_0/enable_chat_by_default_within_system_settings.py
+++ frappe/patches/v10_0/enable_chat_by_default_within_system_settings.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
+	frappe.reload_doc('core', 'doctype', 'language', force=True)
 	frappe.reload_doctype('System Settings')
 	doc = frappe.get_single('System Settings')
 	doc.enable_chat = 1
